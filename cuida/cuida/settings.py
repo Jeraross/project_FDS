8b166aa949325907e8490e0671d1b7a97ec99a42
Henrique Figueiredo <hft@cesar.school>
2024-09-25 17:11:49 -0300

    update settings.py

--- cuida/cuida/settings.py
+++ cuida/cuida/settings.py
@@ -137,3 +137,5 @@ STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
